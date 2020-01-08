c507d3d5eb6d59faa2a4b35110aa108b0948c51b
Edis Skenderi <eskenderi@egamingc.com>
2020-01-08 16:53:47 +0100

    home page finished | need to clean up code

---
+++ custom-carousel-main.js
@@ -0,0 +1,79 @@
+
+        $('#carouselExample .card-body').css({ opacity: 0 });
+
+
+        $('#carouselExample .active').next().mouseover(
+            function () {
+                $(this).find('.card-body').css('opacity', '1').removeClass('zoomOut  animation-duration').addClass('animation-duration zoomIn');
+            }
+        );
+
+        $('#carouselExample .active').next().mouseout(
+            function () {
+                $(this).find(".card-body").css('opacity', '0').removeClass('animation-duration zoomIn').addClass('animation-duration zoomOut ');
+            }
+        );
+
+
+        $('#carouselExample').bind('wheel', function (e) {
+
+            if (e.originalEvent.wheelDelta / 120 < 0) {
+                $(this).carousel('next');
+            } else {
+                $(this).carousel('prev');
+            }
+
+        });
+
+        $('#carouselExample').on('slide.bs.carousel', function (e) {
+
+            var active = $(e.relatedTarget);
+
+            var idx = active.index();
+
+            var itemsPerSlide = 4;
+            var totalItems = $('.carousel-item-main').length;
+
+
+            if (idx >= totalItems - (itemsPerSlide - 1)) {
+
+
+                var it = itemsPerSlide - (totalItems - idx);
+
+                for (var i = 0; i < it; i++) {
+
+                    // append slides to end
+                    if (e.direction == "left") {
+                        $('.carousel-item-main').eq(i).appendTo('.carousel-inner-main');
+                    }
+                    else {
+
+                        $('.carousel-item-main').eq(0).appendTo('.carousel-inner-main');
+                    }
+                }
+            }
+
+
+            $.each($('.carousel-item-main'), function(key,val){
+                
+                $(val).mouseover(function () {
+                    $(this).find('.card-body').css('opacity', '0').removeClass('animation-duration zoomIn').addClass('zoomOut');;
+                });
+                $(val).mouseout(function () {
+                    $(this).find(".card-body").css('opacity', '0').addClass('animation-duration zoomIn').removeClass('animation-duration zoomOut');
+                });
+            });
+
+            $(active.next()).mouseover(function () {
+                $(this).find('.card-body').css('opacity', '1').addClass('animation-duration zoomIn').removeClass('zoomOut');;
+            });
+            $((active).next()).mouseout(function () {
+                $(this).find(".card-body").css('opacity', '0').removeClass('animation-duration zoomIn').addClass('animation-duration zoomOut');
+            });
+
+           
+        });
+
+
+
+       
---
+++ custom-carousel-news.js
@@ -0,0 +1,94 @@
+ $('#recipeCarousel').on('slide.bs.carousel', function (e) {
+
+            /*
+            
+            CC 2.0 License Iatek LLC 2018
+            Attribution required
+            
+            */
+
+            var $e = $(e.relatedTarget);
+            var idx = $e.index();
+            var itemsPerSlide = 2;
+            var totalItems = $('.carousel-item-news').length;
+
+            if (idx >= totalItems - (itemsPerSlide - 1)) {
+                var it = itemsPerSlide - (totalItems - idx);
+                for (var i = 0; i < it; i++) {
+                    // append slides to end
+                    if (e.direction == "left") {
+                        $('.carousel-item-news').eq(i).appendTo('.carousel-inner-news');
+                    } else {
+                        $('.carousel-item-news').eq(0).appendTo('.carousel-inner-news');
+                    }
+
+
+                }
+            }
+        });
+
+        $('.carousel-controls .carousel-control-prev').css({
+            opacity: 0
+        });
+        $(document).ready(function () {
+            var count = $('#recipeCarousel .carousel-inner').children().length
+            var firstSrc = $('#recipeCarousel .carousel-inner .carousel-item:first-child img').attr('src'),
+                lastSrc = $('#recipeCarousel .carousel-inner .carousel-item:nth-child(' + (count - 4) + ') img').attr('src');
+
+
+
+            function showHideButtons() {
+                setTimeout(function () {
+                    if ($('#smallGallery .active img').attr('src') == lastSrc) {
+                        $('.carousel-controls .carousel-control-next').css({
+                            opacity: 0,
+                            'pointer-events': 'none'
+                        });
+                    } else {
+                        $('.carousel-controls .carousel-control-next').css({
+                            opacity: 1,
+                            'pointer-events': 'auto'
+                        });
+                    }
+                    if ($('#smallGallery .active img').attr('src') == firstSrc) {
+                        $('.carousel-controls .carousel-control-prev').css({
+                            opacity: 0,
+                            'pointer-events': 'none'
+                        });
+                    } else {
+                        $('.carousel-controls .carousel-control-prev').css({
+                            opacity: 1,
+                            'pointer-events': 'auto'
+                        });
+                    }
+                }, 700)
+            }
+
+
+
+            $('.carousel-controls .carousel-control-prev,.carousel-controls .carousel-control-next').bind(
+                'click',
+                function (e) {
+                    showHideButtons()
+                })
+
+            $('#recipeCarousel').bind('wheel', function (e) {
+                if ($('#smallGallery .active img').attr('src') != lastSrc) {
+                    if (e.originalEvent.wheelDelta / 120 < 0) {
+                        $(this).carousel('next');
+                    }
+                }
+                if ($('#smallGallery .active img').attr('src') != firstSrc) {
+                    if (e.originalEvent.wheelDelta / 120 > 0) {
+                        $(this).carousel('prev');
+                    }
+                }
+                showHideButtons()
+            });
+
+
+
+        });
+
+
+    
