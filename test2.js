8781fe3bd54a1ae3996648cb3188fd16b93d82fc
Edis Skenderi <eskenderi@egamingc.com>
2020-01-13 14:58:39 +0100

    animate fixed

--- test2.js
+++ test2.js
@@ -2,13 +2,20 @@ $(document).ready(function(){
     var animDuration = 1000;
 
     $("#mainGallery").on( 'click', '.klik', (function () {
-        $('#carouselExample').find('.card-body').css({ height: '0px' });
+        
         $("#mainGallery").find('.active').next().next().nextAll().hide();
+        
+        $("#smallGallery").fadeOut('slow');
+        
+        $('#carouselExample').find('.card-body').animate({ 
+            // transition: 'all 2s ease-in-out',
+            opacity: '0' ,
+        }, 1000);
         
         $("#mainGallery").find('.active').animate({
             transition: 'all .2s ease-in-out',
             right : '31%',
-        }, 3400);
+        }, 2400);
         
         $("#mainGallery").find('.active').nextAll().animate({ left: '33.3333%'}, {
             // transition: 'all 1.2s ease-in-out',
@@ -17,7 +24,7 @@ $(document).ready(function(){
             step: function(now,fx) {
                 $(this).parent().css({
                     // transition: 'all 2.5s linear',
-                    width: '140%',
+                    // width: '140%',
                     'transform-origin': '0 0',
                     
                     // transform: ' scale(1.6, 1)',
@@ -37,24 +44,29 @@ $(document).ready(function(){
             complete: function(){
                 
                $(this).parent().css({
-                   transition: 'all .9s linear',
+
+                transition: 'all .9s linear',
                 'transform-origin': 'left top',
-                   transform: ' scale(1.6)',
-                   left: '-62%',
+                transform: ' scale(1.6)',
+                width: '140%',
+                left: '-62%',
                });
 
+               $('#carouselExample').find('.card-body').animate({ 
+                // transition: 'all 2s ease-in-out',
+                height: '0px' ,
+            }, 1000);
+
                $("#mainGallery").find('.active').nextAll().removeClass('p-0', 1000);
-                
+               
             }
     
-        }, 3400);
+        }, 3400);    
 
-        
-
-        $("#smallGallery").css({
-            // right: "120%",
-            display:'none'
-        });
+        // $("#smallGallery").css({
+        //     // right: "120%",
+        //     display:'none'
+        // });
         
     
     }));
