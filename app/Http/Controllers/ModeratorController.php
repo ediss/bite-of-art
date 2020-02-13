49f41fd0840fe21165834d3839a8e7420fbf538c
ediss <skenderi.e94@gmail.com>
2020-02-13 17:30:33 +0100

    test

---
+++ app/Http/Controllers/ModeratorController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Models\Event;
+use Response;
+use View;
+
+
+class ModeratorController extends Controller
+{
+    //@todo make middleware for moderator
+    public function __construct()
+    {
+        $this->middleware('moderator');
+    }
+
+    public function index() {
+        return view('layout.dashboard');
+    }
+
+    public function getGallerists() {
+        $gallerists = new User;
+
+        $data = $gallerists->where('role_id', '2')->get();
+
+        $html = View::make('inc.partial.dashboard.tables.gallerists-table',[
+            'gallerists'  => $data,
+        ])->render();
+
+        return Response::json(["html" => $html]);
+    }
+
+    public function getEvents() {
+        $events = new Event;
+
+        $data = $events->all();
+
+        $html = View::make('inc.partial.dashboard.tables.events-table',[
+            'events'  => $data,
+        ])->render();
+
+        return Response::json(["html" => $html]);
+    }
+
+    public function updateEvent(Request $request, $event_id) {
+        $success = null;
+        $validator = null;
+        $event = Event::find($event_id);
+
+        // if ($request->isMethod('post')) {
+        //     $validator = Validator::make($request->all(), [
+        //         "new_consultation"  => "required",
+        //         "new_profession"    => "max:60"
+        //     ],
+        //     [
+
+        //         "new_consultation.required" => "Polje 'Konsultacija: ' ne sme biti prazno",
+        //         "new_profession.max"        => "Maksimum broj karaktera koji mozete da uneste za zanimanje je: 60",
+        //     ]);
+
+        //     if ($validator->passes()){
+
+        //         $patient = Patient::find($patient_id);
+
+        //         $patient->address            = $request->input('new_address');
+        //         $patient->phone              = $request->input('new_phone');
+        //         $patient->skype_name         = $request->input('new_skype');
+        //         $patient->profession         = $request->input('new_profession');
+        //         $patient->diagnosis          = $request->input('new_patient_diagnosis');
+        //         $patient->therapy            = $request->input('new_patient_therapy');
+        //         $patient->consultation       = $request->input('new_consultation');
+
+
+        //         if($patient->save()) {
+        //             Session::flash('success', 'Uspesno ste izmenili podatke o pacijentu ');
+        //             $success = true;
+        //         }
+        //     }
+        // }
+
+        $html = View::make('moderator.modals.update-event', [
+
+            'event' => $event,
+            'validator' => $validator
+        ])->render();
+
+        return Response::json( ["html"=>$html, "success" => $success]);
+
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name'          => 'Galerista',
+                'email'         => 'galerista@gmail.com',
+                'password' => bcrypt('12345678'),
+                'role_id'       => 2
+            ],
+            
+            [
+                'name'          => 'moderator',
+                'email'         => 'moderator@gmail.com',
+                'password'      => bcrypt('12345678'),
+                'role_id'       => 1
+            ]
+        ]);
+    }
+}
