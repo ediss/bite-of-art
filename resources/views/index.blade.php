f6307f5652519667eb3fcc5044680167e2d6ad6e
Edis Skenderi <eskenderi@egamingc.com>
2020-03-04 12:51:59 +0100

    main slider change9

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,14 +75,17 @@
 
 
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+
+            <!-- UNDER FOR COMMENT ONLY FOR PRESENTATION -->
+
+            {{-- <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
             <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
-            </a>
+            </a> --}}
         </div>
 
 
@@ -142,7 +145,9 @@
 
 
             </div>
-            <div class="carousel-controls-main">
+
+            <!-- UNDER FOR COMMENT ONLY FOR PRESENTATION -->
+            {{-- <div class="carousel-controls-main">
                 <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
@@ -152,7 +157,7 @@
                     <span class="sr-only">Next</span>
                 </a>
 
-            </div>
+            </div> --}}
         </div>
 
     </div>
