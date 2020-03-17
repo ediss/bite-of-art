22288a09865d2a38bdbc7329be8bfbd9790889e8
Edis Skenderi <eskenderi@egamingc.com>
2020-03-17 11:22:22 +0100

    fixing opened event

--- resources/views/event-opened.blade.php
+++ resources/views/event-opened.blade.php
@@ -102,7 +102,7 @@
 
                 </div>
 
-                @if(isset($desc[1]))
+                @if(isset($desc[1]) && $desc[1] != "")
                 <div class="carousel-item carousel-item-gallery p-0 col-12  col-lg-4 ">
                     @php
                     $counter++;
