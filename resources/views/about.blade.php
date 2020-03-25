4cd0abcb94c61fbafbf8e5db5c5791233b6c0738
Edis Skenderi <eskenderi@egamingc.com>
2020-03-25 13:35:10 +0100

    about new page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,7 @@
 @section('css')
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/animate.css')}}" />
 <link type="text/css" rel="stylesheet" href="{{ asset('assets/css/custom-style.css')}}" />
+
 @endsection
 
 @section('logo-img')
@@ -18,73 +19,155 @@
     @endphp
 
     <div class="col-12 p-0">
-        <div id="carouselExample2" class="carousel slide about-carousel" data-ride="carousel" data-interval="900000">
-            <div class="carousel-inner carousel-inner-gallery  transform-img p-0 m-0 next-id" role="listbox">
+        <div id="myCarouselExampleIndicators" class="carousel slide " data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active  col-lg-8 offset-lg-2 animation-duration2 fadeInUp">
+                    @php
+                    $counter++;
+                    @endphp
+                    <div class="row">
+                        <div class="col-12 about-first-slide  text-justify">
+                            <h1 class="montserrat-bold text-center">About BITE of art</h1>
 
-                <div class="carousel-item carousel-item-gallery  col-1 mobile-hidden col-lg-4  active"
-                    style="visibility:hidden">
+                            <p class="montserrat-regular mt-5">The <b>BITE of Art </b> initiative represents a joint vision
+                                of cultural and artistic institutions and organizations coming from Spain, Slovenia and
+                                Serbia to bring innovative practices into work of cultural operators, and indirectly to
+                                contemporary artists, shaping a path for mainstreaming contemporary art among young
+                                citizens. Our aim is to transform promotion and attractiveness of the contemporary art
+                                among audience by application of the digital communication technologies and the new
+                                approaches in informing and attracting audience, as well as in enhancing artwork
+                                presentation.
+                            </p>
+                            <p class="montserrat-regular">Under the <i>BITE of Art </i> umbrella, we are gathering all
+                                those who can contribute to this goal, on the first place cultural operators,
+                                individuals, various organisations and companies. We started from Serbia, Spain and
+                                Slovenia, with the strong intention to further expand this initiative in Europe.</p>
+                            <p class="montserrat-regular">The European Union supported BITE of Art initiative by
+                                co-financing project for development and piloting unique BITE communication model
+                                through the Creative Europe 2019 programme.</p>
 
-                    <div class="card">
-                        <p>first_slide</p>
-                    </div>
-                </div>
-                @php
-                $counter ++;
-                @endphp
-                <div
-                    class="carousel-item carousel-item-gallery  animation-duration2 fadeInUp col-12  col-lg-4 mobile-first">
-
-                        <div class="row">
-                            <div class="col-10 gallery-first-slide  text-left">
-                                <h1 class="montserrat-bold">About BITE of art</h1>
-
-                                <p class="montserrat-regular">The <b>BITE of Art </b> initiative represents a joint vision of cultural and artistic institutions and organizations coming from Spain, Slovenia and Serbia to bring innovative practices into work of cultural operators, and indirectly to contemporary artists, shaping a path for mainstreaming contemporary art among young citizens. Our aim is to transform promotion and attractiveness of the contemporary art among audience by application of the digital communication technologies and the new approaches in informing and attracting audience, as well as in enhancing artwork presentation.
-                                </p>
-                                <p class="montserrat-regular">Under the <i>BITE of Art </i> umbrella, we are gathering all those who can contribute to this goal, on the first place cultural operators, individuals, various organisations and companies. We started from Serbia, Spain and Slovenia, with the strong intention to further expand this initiative in Europe.</p>
-                                <p class="montserrat-regular">The European Union supported BITE of Art initiative by co-financing project for development and piloting unique BITE communication model through the Creative Europe 2019 programme.</p>
-                                {{-- <div class=" social-scale">
-                        <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u={{ url()->current() }}"
-                                class="fb-xfbml-parse-ignore">
-                                <img class="" src="{{ asset('images/social-network/facebook.png') }}">
-                                </a>
-
-                                <a target="_blank"
-                                    href="http://pinterest.com/pin/create/button/?url={{url()->current()}}&media={{ url($data->event_cover) }}"
-                                    class="pin-it-button" count-layout="horizontal">
-                                    <img class="" src="{{ asset('images/social-network/pinterest.png') }}">
-                                </a>
-
-                                <a target="_blank"
-                                    href="http://twitter.com/share?&url={{url()->current()}}&hashtags=bite,of,art">
-                                    <img class="" src="{{ asset('images/social-network/twitter.png') }}">
-                                </a>
-
-
-                                <a target="_blank"
-                                    href="http://www.linkedin.com/shareArticle?mini=true&url={{ url()->current() }}&title={{ $data->event_name }}&summary={{ url($data->event_cover) }}">
-                                    <img class="" src="{{ asset('images/social-network/linkedin.png') }}">
-                                </a>
-
-                                <a href="mailto:?subject=I wanted you to see this site&amp;body={{url()->current()}}"
-                                    title="Event: {{ $data->event_name }}">
-                                    <img class="mt-2" src="{{ asset('images/social-network/gmail.png') }}">
-                                </a>
-
-                            </div> --}}
                         </div>
 
                     </div>
+                </div>
 
-            </div>
+                <div class="carousel-item col-6 offset-3 ">
+                    @php
+                    $counter++;
+                    @endphp
+
+                    <div class="row">
+                        <div class="col-12">
+                            <a target="_blank" href="https://ec.europa.eu/programmes/creative-europe/node_en">
+                                <img class="img-fluid  d-block gallery-img about-europe"
+                                src         = "{{ asset('images/about/partners-cb/creative_europe.png') }}"
+                                data-src    = "{{ asset('images/about/partners-cb/creative_europe.png') }}"
+                                data-hover  = "{{ asset('images/about/partners/creative_europe.png') }}"
+                                alt="slide 2">
+                            </a>
 
 
-            <div class="carousel-item carousel-item-gallery p-0 col-12  col-lg-4 ">
-                @php
-                $counter++;
-                @endphp
-                <div class="card">
-                    <img class="img-fluid  d-block gallery-img" src="{{ asset('images/about/BITEAB.jpg') }}"
-                        alt="slide 2">
+
+                        </div>
+                    </div>
+                    <div class="row mb-5">
+                        <div class="col-4">
+                            <a target="_blank" href="http://bum.org.rs/">
+                                <img class="img-fluid  d-block gallery-img about-bum"
+                                    src         = "{{ asset('images/about/partners-cb/BUM.png') }}"
+                                    data-src    = "{{ asset('images/about/partners-cb/BUM.png') }}"
+                                    data-hover  = "{{ asset('images/about/partners/BUM.png') }}"
+                                    alt="slide 2">
+                            </a>
+
+                        </div>
+
+                        <div class="col-4"></div>
+                        
+                        <div class="col-4">
+                            <a target="_blank" href="https://www.tipovej.org/">
+                                <img class="img-fluid  d-block gallery-img about-tripovej"
+                                    src         = "{{ asset('images/about/partners-cb/Tipovej_Slovenia.png') }}"
+                                    data-src    = "{{ asset('images/about/partners-cb/Tipovej_Slovenia.png') }}"
+                                    data-hover  = "{{ asset('images/about/partners/Tipovej_Slovenia.png') }}"
+                                    alt="slide 2">
+                            </a>
+
+                            <a target="_blank" href="https://www.mladizmaji.si/en/">
+                                <img class="img-fluid  d-block gallery-img about-zmaji"
+                                    src         = "{{ asset('images/about/partners-cb/mladi_zmaji.png') }}"
+                                    data-src    = "{{ asset('images/about/partners-cb/mladi_zmaji.png') }}"
+                                    data-hover  = "{{ asset('images/about/partners/mladi_zmaji.png') }}"
+                                    alt="slide 2">
+                            </a>
+                     
+
+                        
+                        </div>
+                    </div>
+
+                    <div class="row mt-5">
+                        <div class="col-4 ">
+                            <a target="_blank" href="http://www.backslash.es/">
+                                <img class="img-fluid  d-block gallery-img about-blackslash"
+                                    src         = "{{ asset('images/about/partners-cb/Backslash.png') }}"
+                                    data-src    = "{{ asset('images/about/partners-cb/Backslash.png') }}"
+                                    data-hover  = "{{ asset('images/about/partners/Backslash.png') }}"
+                                    alt="slide 2">
+                            </a>
+                        </div>
+                        <div class="col-4"></div>
+                        <div class="col-4">
+                            <a target="_blank" href="http://communications.rs/">
+                                <img class="img-fluid  d-block gallery-img about-colormedia"
+                                    src         = "{{ asset('images/about/partners-cb/color_media.png') }}"
+                                    data-src    = "{{ asset('images/about/partners-cb/color_media.png') }}"
+                                    data-hover  = "{{ asset('images/about/partners/color_media.png') }}"
+                                    alt="slide 2">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="row ">
+                        <div class="col-4 ">
+                            <a target="_blank" href="http://connect-international.org/en/">
+                                <img class="img-fluid  d-block gallery-img about-connect"
+                                    src         = "{{ asset('images/about/partners-cb/Connect_logo.png') }}"
+                                    data-src    = "{{ asset('images/about/partners-cb/Connect_logo.png') }}"
+                                    data-hover  = "{{ asset('images/about/partners/Connect_logo.png') }}"
+                                    alt="slide 2">
+                            </a>
+
+                            <a target="_blank" href="https://www.kcb.org.rs/">
+                                <img class="img-fluid  d-block gallery-img about-kcb"
+                                    src         = "{{ asset('images/about/partners-cb/Kulturni_centar_BGD.png') }}"
+                                    data-src    = "{{ asset('images/about/partners-cb/Kulturni_centar_BGD.png') }}"
+                                    data-hover  = "{{ asset('images/about/partners/Kulturni_centar_BGD.png') }}"
+                                    alt="slide 2">
+                            </a>
+                        </div>
+                        <div class="col-4"></div>
+                        <div class="col-4">
+                            <a target="_blank" href="https://www.consorcimuseus.gva.es/">
+                                <img class="img-fluid  d-block gallery-img about-valencia"
+                                    src         = "{{ asset('images/about/partners-cb/Valencia_Museums.png') }}"
+                                    data-src    = "{{ asset('images/about/partners-cb/Valencia_Museums.png') }}"
+                                    data-hover  = "{{ asset('images/about/partners/Valencia_Museums.png') }}"
+                                    alt="slide 2">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item col-8 offset-2 ">
+                    @php
+                    $counter++;
+                    @endphp
+                   
+                        <img class="img-fluid  d-block gallery-img" src="{{ asset('images/about/BITEAB.jpg') }}"
+                            alt="slide 2">
+
+                  
 
                 </div>
 
@@ -92,7 +175,13 @@
 
         </div>
 
+        <!-- UNDER FOR COMMENT ONLY FOR PRESENTATION -->
+
+
     </div>
+
+
+</div>
 
 </div>
 
@@ -102,17 +191,17 @@
 </div>
 
 <div class="carousel-controls-main">
-    <a class="carousel-control-prev" href="#carouselExample2" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExample2" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+           <a class="carousel-control-prev" href="#myCarouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     <ol class="carousel-indicators">
 
-        @for ($i = 0; $i < $counter; $i++) <li data-target="#carouselExample2" data-slide-to="{{ $i }}"
+        @for ($i = 0; $i < $counter; $i++) <li data-target="#myCarouselExampleIndicators" data-slide-to="{{ $i }}"
             {{ ($i == 0) ? "class=active" : ""}}>
             </li>
             @endfor
@@ -122,8 +211,13 @@
 @endsection
 
 @section('footer-scripts')
+
+<script src=" {{ asset('assets/js/opened-event.js') }}"></script>
+
+<script src=" {{ asset('assets/js/about.js') }}"></script>
+
 <script>
     $('.footer').css('display', 'none');
 </script>
-<script src=" {{ asset('assets/js/opened-news.js') }}"></script>
+
 @endsection
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -131,23 +131,23 @@
                 <div class="row languages">
                     <div class="col-6 offset-3 col-md-4 offset-md-4 montserrat-regular">
                         <div class="menu-footer text-center login">
-                           
-                            
+
+
                             @foreach (config('app.available_languages') as $language)
                             @if(\Illuminate\Support\Facades\Route::currentRouteName() == "opened.event" || \Illuminate\Support\Facades\Route::currentRouteName() == "opened.news")
                             <a href="{{ route(\Illuminate\Support\Facades\Route::currentRouteName(), ['language'=> $language, 'id' => $url_id] ) }}" class=" {{(app()->getLocale() == $language) ? 'active-language' : ''}} ">
                                 {{strtoupper($language)}}
                             </a>
-                            
+
                             @else
                             <a href="{{ route(\Illuminate\Support\Facades\Route::currentRouteName(), $language ) }}" class=" {{(app()->getLocale() == $language) ? 'active-language' : ''}} ">
                                 {{strtoupper($language)}}
                             </a>
                             @endif
-                            
+
                             <span class="ml-1 mr-1 menu-span">|</span>
                             @endforeach
-                            
+
 
                         </div>
                     </div>
