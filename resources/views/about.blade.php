43c38db2ff7f8f9f30c89e16e9199e4db1deba73
Edis Skenderi <eskenderi@egamingc.com>
2020-03-30 21:03:54 +0200

    new about slide some changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -56,7 +56,7 @@
                     @endphp
 
                     <div class="row">
-                        <div class="col-8 offset-2">
+                        <div class="col-6 offset-3">
                             <div class="row">
                                 <div class="col-12">
                                     <a target="_blank" href="https://ec.europa.eu/programmes/creative-europe/node_en">
