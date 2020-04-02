70a574566bd8914492d3285d698cd9f1838f8601
Edis Skenderi <eskenderi@egamingc.com>
2020-04-02 15:30:05 +0200

    fixing arrows of news on homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -201,20 +201,27 @@
                 @endforeach
 
             </div>
-            <a class="carousel-control-prev" href="#recipeCarousel" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#recipeCarousel" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
+    
         </div>
 
     </div>
+
     {{-- <div class="carousel-controls">
 
     </div> --}}
+</div>
+<div class="row">
+    <div class="col-12">
+        <a class="carousel-control-prev" href="#recipeCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#recipeCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    
 </div>
 <div class="row h-80px" id="news-h1">
     <div class="col-4"></div>
